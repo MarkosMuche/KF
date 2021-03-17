7d888d1d774996eb759052cc7888c878834bf40e
Markos Amsalu Muche <markosmuche2018@gmail.com>
2021-03-17 03:39:31 -0700

    Update kalman_example.ino

--- kalman_example/kalman_example.ino
+++ kalman_example/kalman_example.ino
@@ -2,26 +2,23 @@
 #include <KF.h>
 
 KF kf;
-float x[2] = {1, 1};
-float P[4] = {0 , 1 , 1, 1};
-float x[2] = {1, 1};
-float P[4] = {0 , 1 , 1, 1};
+float x[2] = {0.1, 0.1};
+float P[4] = {0.1 , 0, 0, 0.1};
 float  A[4] = {1, 0.1, 0 , 1};
 float B[2] = {0 , 1};
 float C[2] = {1, 0};
-float Q[4]={1, 0, 0, 10};
-float R=1;
-
+float Q[4] = {1, 0, 0, 10};
+float R = 1;
 void setup() {
   Serial.begin(9600);
+  kf.getABC(A, B, C);
+  kf.getQR(Q, R);
 }
 
 void loop() {
-float r[2];
-kf.getABC(A,B,C);
-kf.kalman(x,P,1,1);
-kf.getQR(Q,R);  
-Serial.print(x[0]);
- Serial.print(x[1]);
-   
+  float z = 1;
+  float u = 1;
+  kf.kalman(x, P, u, z);
+  Serial.print(x[0]);
+  Serial.print(x[1]);
 }
