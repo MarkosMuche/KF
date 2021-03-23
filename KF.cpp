d84d86fa96e3c3f86960e3183bcde32695862c6b
Markos Amsalu Muche <markosmuche2018@gmail.com>
2021-03-23 09:42:02 -0700

    Update KF.cpp

--- KF.cpp
+++ KF.cpp
@@ -1,8 +1,6 @@
 
 #include "Arduino.h"
 #include "KF.h"
-
-
 KF::KF() {
 }
 
@@ -25,50 +23,33 @@ void KF::getQR(float Q[4], float R){
   this->_R=R;
 }
 //Kalmanfiltethis->_R
-float* KF::kalman(float x[2], float P[4],float u,float z)
+void KF::kalman(float x[2], float P[4],float u,float z)
 {
-
+    //note that u is the gyro reading and z is the accelerometer reading
     float  S;
     float L1;
     float  L2;
     float I1 = 1;
     float I2 = 1;
-  float x1 = x[0];
-  float x2 = x[1];
-  float p11 = P[0];
-  float p12 = P[1];
-  float p21 = P[2];
-  float p22 = P[3];
-  //timeupdate
-  float x1p = this->_A[0] * x1 + this->_A[1] * x2 + this->_B[0] * u;
-  float x2p = this->_A[2] * x1 + this->_A[3] * x2;
-
-  float p11p = (this->_A[0]) * (this->_A[0] * p11 + this->_A[1] * p21) + (this->_A[1]) * (this->_A[0] * p12 + this->_A[1] * p22)+this->_Q[1];
-  float p12p = (this->_A[2]) * (this->_A[0] * p11 + this->_A[1] * p21) + (this->_A[3]) * (this->_A[0] * p12 + this->_A[1] * p22)+this->_Q[2];
-  float p21p = (this->_A[0]) * (this->_A[2] * p11 + this->_A[3] * p21) + (this->_A[1]) * (this->_A[2] * p12 + this->_A[3] * p22)+this->_Q[3];
-  float p22p = (this->_A[2]) * (this->_A[2] * p11 + this->_A[3] * p21) + (this->_A[3]) * (this->_A[2] * p12 + this->_A[3] * p22)+this->_Q[4];
-
+    //time update
+  float x1p = this->_A[0] * x[0] + this->_A[1] * x[1] + this->_B[0] * u;
+  float x2p = this->_A[2] * x[0] + this->_A[3] * x[1]+this->_B[1]*u;
+  
+  float P11p = (this->_A[0]) * (this->_A[0] * P[0] + this->_A[1] * P[2]) + (this->_A[1]) * (this->_A[0] * P[1] + this->_A[1] * P[3])+this->_Q[1];
+  float P12p = (this->_A[2]) * (this->_A[0] * P[0] + this->_A[1] * P[2]) + (this->_A[3]) * (this->_A[0] * P[1] + this->_A[1] * P[3])+this->_Q[2];
+  float P21p = (this->_A[0]) * (this->_A[2] * P[0] + this->_A[3] * P[2]) + (this->_A[1]) * (this->_A[2] * P[1] + this->_A[3] * P[3])+this->_Q[3];
+  float P22p = (this->_A[2]) * (this->_A[2] * P[0] + this->_A[3] * P[2]) + (this->_A[3]) * (this->_A[2] * P[1] + this->_A[3] * P[3])+this->_Q[4];
   // calculate kalman gain
   S = (this->_C[0]) * (this->_C[0] * p11p + this->_C[1] * p21p) + (this->_C[1]) * (this->_C[0] * p12p + this->_C[1] * p22p) + this->_R ;
   S = 1 / S;
-  L1 = (p11p * (this->_C[0]) + p12p * (this->_C[1])) * S;
-  L2 = ( p21p * (this->_C[0]) + p22p * (this->_C[1])) * S;
+  L1 = (p11p * this->_C[0] + p12p * this->_C[1]) * S;
+  L2 = ( p21p * this->_C[0] + p22p * this->_C[1]) * S;
+  // measuement update
+  x[0] = x1p + L1 * (z - this->_C[0] * x1p - this->_C[1] * x2p);
+  x[1] = x2p + L2 * (z - this->_C[0] * x1p - this->_C[1] * x2p);
 
-  // time update
-  x1 = x1p + L1 * (z - this->_C[0] * x1p - this->_C[1] * x2p);
-  x2 = x2p + L2 * (z - this->_C[0] * x1p - this->_C[1] * x2p);
-
-  p11 = (I1 - L1 * this->_C[0]) * p11p - L1 * this->_C[1] * p21p;
-  p12 = (I1 - L1 * this->_C[0]) * p12p - L1 * this->_C[1] * p22p;
-  p21 = -L2 * this->_C[0] * p11p + (I2 - L2 * this->_C[1]) * p21p;
-  p22 = -L2 * this->_C[0] * p12p + (I2 - L2 * this->_C[1]) * p22p;
-
-x[0]=x1;
-x[1]=x2;
-
- P[0]=p11;
- P[1]=p12;
- P[2]=p21;
- P[3]=p22;
- 
+  P[0] = (I1 - L1 * this->_C[0]) * p11p - L1 * this->_C[1] * p21p;
+  P[1] = (I1 - L1 * this->_C[0]) * p12p - L1 * this->_C[1] * p22p;
+  P[2] = -L2 * this->_C[0] * p11p + (I2 - L2 * this->_C[1]) * p21p;
+  P[3] = -L2 * this->_C[0] * p12p + (I2 - L2 * this->_C[1]) * p22p;
 }
